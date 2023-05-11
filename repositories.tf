79e25e593148507979da1a65710929acc2a8efea
Romain Cambonie <romain.cambonie@gmail.com>
2023-05-11 22:23:18 +0200

    feat: added all infrastructure template definitions

--- repositories.tf
+++ repositories.tf
@@ -6,28 +6,371 @@ module "aws_service_vpc_template" {
   github_repository   = "aws-service-vpc"
 
   files = {
-    workflow_apply = {
+    workflow-apply = {
       path            = ".github/workflows/apply.terraform.yml"
       url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/apply-terraform-aws-service-infrastructure.yml"
       template_variables = {
         SERVICE = "vpc-infrastructure"
       }
     }
-    workflow_sync = {
+    workflow-sync = {
       path            = ".github/workflows/sync-with-upstream.yml"
       url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/sync-with-upstream.yml"
       template_variables = {
         UPSTREAM = "codingones-github-templates/aws-service-vpc"
       }
     }
-    vpc = {
-      path               = "vpc.tf"
-      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/main/terraform/service/vpc/vpc.tf"
-      template_variables = {}
-    },
     outputs = {
       path               = "outputs.tf"
       url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/vpc/outputs.tf"
+      template_variables = {}
+    }
+    vpc = {
+      path               = "vpc.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/vpc/vpc.tf"
+      template_variables = {}
+    }
+  }
+
+  providers = {
+    github = github
+  }
+}
+
+module "aws_service_domain_template" {
+  source = "github.com/codingones-terraform-modules/aws-service-infrastructure-template-repository-generator"
+
+  service             = "domain"
+  github_organization = "codingones-github-templates"
+  github_repository   = "aws-service-domain"
+
+  files = {
+    workflow-apply = {
+      path            = ".github/workflows/apply.terraform.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/apply-terraform-aws-service-infrastructure.yml"
+      template_variables = {
+        SERVICE = "domain-infrastructure"
+      }
+    }
+    workflow-sync = {
+      path            = ".github/workflows/sync-with-upstream.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/sync-with-upstream.yml"
+      template_variables = {
+        UPSTREAM = "codingones-github-templates/aws-service-domain"
+      }
+    }
+    acm = {
+      path               = "acm.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/domain/route53.tf"
+      template_variables = {}
+    }
+    outputs = {
+      path               = "outputs.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/domain/outputs.tf"
+      template_variables = {}
+    }
+    route53 = {
+      path               = "route53.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/domain/route53.tf"
+      template_variables = {}
+    }
+  }
+
+  providers = {
+    github = github
+  }
+}
+
+module "aws_service_email_template" {
+  source = "github.com/codingones-terraform-modules/aws-service-infrastructure-template-repository-generator"
+
+  service             = "email"
+  github_organization = "codingones-github-templates"
+  github_repository   = "aws-service-email"
+
+  files = {
+    workflow-apply = {
+      path            = ".github/workflows/apply.terraform.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/apply-terraform-aws-service-infrastructure.yml"
+      template_variables = {
+        SERVICE = "email-infrastructure"
+      }
+    }
+    workflow-sync = {
+      path            = ".github/workflows/sync-with-upstream.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/sync-with-upstream.yml"
+      template_variables = {
+        UPSTREAM = "codingones-github-templates/aws-service-email"
+      }
+    }
+    iam = {
+      path               = "iam.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/email/iam.tf"
+      template_variables = {}
+    }
+    lambda = {
+      path               = "lambda.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/email/lambda.tf"
+      template_variables = {}
+    }
+    outputs = {
+      path               = "outputs.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/email/outputs.tf"
+      template_variables = {}
+    }
+    route53 = {
+      path               = "route53.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/email/route53.tf"
+      template_variables = {}
+    }
+    s3 = {
+      path               = "s3.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/email/s3.tf"
+      template_variables = {}
+    }
+    ses = {
+      path               = "ses.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/email/ses.tf"
+      template_variables = {}
+    }
+  }
+
+  providers = {
+    github = github
+  }
+}
+
+module "aws_service_identity_template" {
+  source = "github.com/codingones-terraform-modules/aws-service-infrastructure-template-repository-generator"
+
+  service             = "identity"
+  github_organization = "codingones-github-templates"
+  github_repository   = "aws-service-identity"
+
+  files = {
+    workflow-apply = {
+      path            = ".github/workflows/apply.terraform.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/apply-terraform-aws-service-infrastructure.yml"
+      template_variables = {
+        SERVICE = "identity-infrastructure"
+      }
+    }
+    workflow-sync = {
+      path            = ".github/workflows/sync-with-upstream.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/sync-with-upstream.yml"
+      template_variables = {
+        UPSTREAM = "codingones-github-templates/aws-service-identity"
+      }
+    }
+    cognito = {
+      path               = "cognito.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/identity/cognito.tf"
+      template_variables = {}
+    }
+    iam = {
+      path               = "iam.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/identity/iam.tf"
+      template_variables = {}
+    }
+    outputs = {
+      path               = "outputs.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/identity/outputs.tf"
+      template_variables = {}
+    }
+    sns = {
+      path               = "sns.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/identity/sns.tf"
+      template_variables = {}
+    }
+  }
+
+  providers = {
+    github = github
+  }
+}
+
+module "aws_service_persistence_template" {
+  source = "github.com/codingones-terraform-modules/aws-service-infrastructure-template-repository-generator"
+
+  service             = "persistence"
+  github_organization = "codingones-github-templates"
+  github_repository   = "aws-service-persistence"
+
+  files = {
+    workflow-apply = {
+      path            = ".github/workflows/apply.terraform.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/apply-terraform-aws-service-infrastructure.yml"
+      template_variables = {
+        SERVICE = "persistence-infrastructure"
+      }
+    }
+    workflow-sync = {
+      path            = ".github/workflows/sync-with-upstream.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/sync-with-upstream.yml"
+      template_variables = {
+        UPSTREAM = "codingones-github-templates/aws-service-persistence"
+      }
+    }
+    outputs = {
+      path               = "outputs.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/persistence/outputs.tf"
+      template_variables = {}
+    }
+    rds = {
+      path               = "rds.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/persistence/rds.tf"
+      template_variables = {}
+    }
+    vpc = {
+      path               = "vpc.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/persistence/vpc.tf"
+      template_variables = {}
+    }
+  }
+
+  providers = {
+    github = github
+  }
+}
+
+module "aws_service_registry_template" {
+  source = "github.com/codingones-terraform-modules/aws-service-infrastructure-template-repository-generator"
+
+  service             = "registry"
+  github_organization = "codingones-github-templates"
+  github_repository   = "aws-service-registry"
+
+  files = {
+    workflow-apply = {
+      path            = ".github/workflows/apply.terraform.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/apply-terraform-aws-service-infrastructure.yml"
+      template_variables = {
+        SERVICE = "registry-infrastructure"
+      }
+    }
+    workflow-sync = {
+      path            = ".github/workflows/sync-with-upstream.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/sync-with-upstream.yml"
+      template_variables = {
+        UPSTREAM = "codingones-github-templates/aws-service-registry"
+      }
+    }
+    ecr = {
+      path               = "ecr.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/registry/ecr.tf"
+      template_variables = {}
+    }
+    outputs = {
+      path               = "outputs.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/registry/outputs.tf"
+      template_variables = {}
+    }
+  }
+
+  providers = {
+    github = github
+  }
+}
+
+module "aws_service_api_template" {
+  source = "github.com/codingones-terraform-modules/aws-service-infrastructure-template-repository-generator"
+
+  service             = "api"
+  github_organization = "codingones-github-templates"
+  github_repository   = "aws-service-api"
+
+  files = {
+    workflow-apply = {
+      path            = ".github/workflows/apply.terraform.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/apply-terraform-aws-service-infrastructure.yml"
+      template_variables = {
+        SERVICE = "api-infrastructure"
+      }
+    }
+    workflow-sync = {
+      path            = ".github/workflows/sync-with-upstream.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/sync-with-upstream.yml"
+      template_variables = {
+        UPSTREAM = "codingones-github-templates/aws-service-api"
+      }
+    }
+    alb = {
+      path               = "alb.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/api/alb.tf"
+      template_variables = {}
+    }
+    api-gateway = {
+      path               = "api-gateway.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/api/api-gateway.tf"
+      template_variables = {}
+    }
+    ecs = {
+      path               = "ecs.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/api/ecs.tf"
+      template_variables = {}
+    }
+    iam = {
+      path               = "iam.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/api/iam.tf"
+      template_variables = {}
+    }
+    outputs = {
+      path               = "outputs.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/api/outputs.tf"
+      template_variables = {}
+    }
+    vpc = {
+      path               = "vpc.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/api/vpc.tf"
+      template_variables = {}
+    }
+  }
+
+  providers = {
+    github = github
+  }
+}
+
+module "aws_service_client_template" {
+  source = "github.com/codingones-terraform-modules/aws-service-infrastructure-template-repository-generator"
+
+  service             = "client"
+  github_organization = "codingones-github-templates"
+  github_repository   = "aws-service-client"
+
+  files = {
+    workflow-apply = {
+      path            = ".github/workflows/apply.terraform.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/apply-terraform-aws-service-infrastructure.yml"
+      template_variables = {
+        SERVICE = "client-infrastructure"
+      }
+    }
+    workflow-sync = {
+      path            = ".github/workflows/sync-with-upstream.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/sync-with-upstream.yml"
+      template_variables = {
+        UPSTREAM = "codingones-github-templates/aws-service-client"
+      }
+    }
+    cloudfront = {
+      path               = "cloudfront.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/client/cloudfront.tf"
+      template_variables = {}
+    }
+    outputs = {
+      path               = "outputs.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/client/outputs.tf"
+      template_variables = {}
+    }
+    route53 = {
+      path               = "route53.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/client/route53.tf"
+      template_variables = {}
+    }
+    s3 = {
+      path               = "s3.tf"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/terraform/service/client/s3.tf"
       template_variables = {}
     }
   }
