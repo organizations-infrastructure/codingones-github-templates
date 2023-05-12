cc0a03e6d66dbe5e334b5c3e5e16f894b424f681
Romain Cambonie <romain.cambonie@gmail.com>
2023-05-12 14:01:52 +0200

    feat: adding aws application client angular template

--- applications-repositories.tf
+++ applications-repositories.tf
@@ -26,3 +26,37 @@ module "aws_application_fastify_api" {
     github = github
   }
 }
+
+module "aws_application_client_angular" {
+  source = "github.com/codingones-terraform-modules/aws-client-template-repository-generator"
+
+  service             = "s3"
+  github_organization = "codingones-github-templates"
+  github_repository   = "aws-application-client-angular"
+
+  files = {
+    workflow-deploy = {
+      path            = ".github/workflows/deploy-to-s3.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/deploy-to-s3.yml"
+      template_variables = {
+        SERVICE = "s3"
+      }
+    }
+    workflow-sync = {
+      path            = ".github/workflows/sync-with-upstream.yml"
+      url_of_template = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/sync-with-upstream.yml"
+      template_variables = {
+        UPSTREAM = "codingones-github-templates/aws-application-client-angular"
+      }
+    }
+    workflow-validate = {
+      path               = ".github/workflows/validate.yml"
+      url_of_template    = "https://raw.githubusercontent.com/codingones-github-templates/files-templates/main/github-actions/validate.yml"
+      template_variables = {}
+    }
+  }
+
+  providers = {
+    github = github
+  }
+}
